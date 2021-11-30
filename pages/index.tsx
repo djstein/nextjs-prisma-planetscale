a9e360355244f5267e5e43a0cf44a6bd0859797b
Dylan Stein <dylan@Dylans-MBP.webpass.net>
2021-11-29 21:44:55 -0800

    jit with dark and light

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,10 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <>
-      <h1 className="text-2xl">Hello</h1>
-      <h2 className="text-xl">World</h2>
-    </>
+    <div className="m-8">
+      <h1 className="text-2xl dark:text-white">Hello</h1>
+      <h2 className="text-xl dark:text-white">World</h2>
+    </div>
   );
 };
 
